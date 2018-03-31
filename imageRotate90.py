657d9cf44e383c55713aa9f3407d2558263a2885
Deemarc Burakitbumrung <Deemarc@thaigertec.local>
2018-03-31 13:58:53 +0700

    add image preparation scirpt

---
+++ imageRotate90.py
@@ -0,0 +1,39 @@
+__copyright__ = """
+
+    Copyright [2018] [MR.Deemarc Burakitbumrung]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+"""
+__license__ = "Apache 2.0"
+
+'''
+rotata all image in the specified folder 90 degree CCW
+'''
+
+import glob
+from PIL import Image
+import os
+def imageRotataAll(path,degree):
+    # get all the video from video_in folder
+    fileReg = path + '/*.jpg'
+    images = glob.glob(fileReg)
+    for image in images:
+        filename_ext = os.path.basename(image)
+        img = Image.open(image)
+        img = img.rotate(degree)
+        img.save(image)
+
+if __name__ == '__main__' :
+    imageRotataAll('./img/deemarc_62',90)
+        
---
+++ video2imgSeq.py
@@ -0,0 +1,74 @@
+__copyright__ = """
+
+    Copyright [2018] [MR.Deemarc Burakitbumrung]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+"""
+__license__ = "Apache 2.0"
+
+'''
+converting video into image sequence
+'''
+
+import cv2
+import numpy as np
+import os
+import glob
+
+# Find OpenCV version
+(major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+
+# get all the video from video_in folder
+videos = glob.glob('./video_in/*.mp4')
+
+try:
+    if not os.path.exists('img'):
+        os.makedirs('img')
+except OSError:
+    print ('Error: Creating directory of data')
+
+for video in videos:
+    filename_ext = os.path.basename(video)
+    filename, file_extension  = os.path.splitext(filename_ext)
+    output_folder = './img/' + filename
+    try:
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+    except OSError:
+        print ('Error: Creating directory of data')
+
+    # get video handle
+    cap = cv2.VideoCapture(video)
+    if int(major_ver)  < 3 :
+        fps = cap.get(cv2.cv.CV_CAP_PROP_FPS)
+        print("Frames per second using video.get(cv2.cv.CV_CAP_PROP_FPS): {0}".format(fps))
+    else :
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        print("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps))
+
+    #for now let's use just 1 second of video
+    second = 1
+    num_img = int(round(fps*second))
+    print("number of img: " + str(num_img))
+
+    for i in range(1,num_img+1):
+        ret, frame = cap.read()
+        output_filename = output_folder +'/'+ filename + '_' + str(i)+'.jpg'
+        print ('Creating...' + output_filename)
+        cv2.imwrite(output_filename, frame)
+
+    
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
