701fa3bb9834025a914ec7e0f94b09fb81d55591
Deemarc Burakitbumrung <Deemarc@thaigertec.local>
2018-05-01 14:41:29 +0700

    add code for haar face detection

--- hr_kernel.py
+++ hr_kernel.py
@@ -6,44 +6,51 @@ import glob
 from detect_face import haarcc_faceDetect
 from detect_human_skin import skinDetector
 
-# cropRatio = [x_left,y_top,x_right,y_bottom]
-def roi_faceDetect(img_in):
-    gray_img = cv2.cvtColor(img_in,cv2.COLOR_BGR2GRAY)
-    people = haarcc_faceDetect.detect(gray_img)
-    # if more than one person has been detected throw an error!
-    if len(people) > 1:
-        raise ValueError('more than one person has been detected in an image!!!')
-    
-    person = people[0]
-    x = person.face[0]
-    y = person.face[1]
-    w = person.face[2]
-    h = person.face[3]
-    roi_img = img_in[y:y+h, x:x+w]
-    return roi_img
-    
-def roi_manual(img_in, cropRatio = [0.36, 0.4, 0.45, 0.10], cvRead=True):
-    h,w = img_in.shape[:2]
+faceDet_al = {'manual':0, 'haar':1}
+# when choosing to detect face manually please congig 'face_cropRatio'
+# it is a percentage ration that you want to remove from left top right down
+face_cropRatio = [0.36, 0.4, 0.45, 0.10]
 
-    x_left = cropRatio[0]*w
-    y_top = cropRatio[1]*h
+def faceDetect(img_in, algor=faceDet_al['manual']):
+    img_x,img_y = img_in.shape[:2]
+    if algor == faceDet_al['manual']:
+        x = face_cropRatio[0]*img_x
+        y = face_cropRatio[1]*img_y
+        w = img_x - face_cropRatio[2]*img_x
+        h = img_y - face_cropRatio[3]*img_y
+    elif algor == faceDet_al['haar']:
+        gray_img = cv2.cvtColor(img_in,cv2.COLOR_BGR2GRAY)
+        people = haarcc_faceDetect.detect(gray_img)
+        # if more than one person has been detected throw an error!
+        if len(people) > 1:
+            raise ValueError('more than one person has been detected in an image!!!')
 
-    x_right = w - cropRatio[2]*w
-    y_bottom = h - cropRatio[3]*h
+        person = people[0]
+        x = person.face[0]
+        y = person.face[1]
+        w = person.face[2]
+        h = person.face[3]
+    else:
+        raise ValueError('faceDet_al value cannot be recognise')
 
-    if cvRead:
-        img_in = cv2.cvtColor(img_in, cv2.COLOR_BGR2RGB)
+    return [x,y,w,h]
 
-    roi_img = img_in[int(y_top):int(y_bottom), int(x_left):int(x_right), :]
-    return roi_img
+def roi_draw(img_in, x,y,w,h):
+    roi_draw_img = img_in
+    cv2.rectangle(roi_draw_img,(x,y),(x+w,y+h),(0,0,255),2)
+    return roi_draw_img
+
+def roi_crop(img_in, x,y,w,h):
+    roi_crop_img = img_in[y:y+h, x:x+w]
+    return roi_crop_img
 
 def colorSpaceCovert(img_in):
     #let's try lot's of color space
-    hls_img = cv2.cvtColor(img_in, cv2.COLOR_RGB2HLS)
-    hsv_img = cv2.cvtColor(img_in, cv2.COLOR_RGB2HSV)
-    lab_img = cv2.cvtColor(img_in, cv2.COLOR_RGB2Lab)
+    hls_img = cv2.cvtColor(img_in, cv2.COLOR_BGR2HLS)
+    hsv_img = cv2.cvtColor(img_in, cv2.COLOR_BGR2HSV)
+    lab_img = cv2.cvtColor(img_in, cv2.COLOR_BGR2Lab)
 
-    img_dict = {'rgb_img':img_in, 'hls_img':hls_img, 'hsv_img':hsv_img, 'lab_img':lab_img}
+    img_dict = {'bgr_img':img_in, 'hls_img':hls_img, 'hsv_img':hsv_img, 'lab_img':lab_img}
     # img_list.append(hls_img)
     # img_list.append(hsv_img)
     # img_list.append(lab_img)
@@ -87,22 +94,34 @@ def cal_mm(img_in, skinMask, com3=True, enb_skinMask = True):
     return mean, median
 
 def kernel(img_in, seq_arr, cvRead=True):
-    # finding roi
-    # roi_img = roi_manual(img_in)
-    roi_img = roi_faceDetect(img_in)
-    skinMask = skinDetector.detect(roi_img)
+    
+    # face detection
+    x,y,w,h = faceDetect(img_in,faceDet_al['haar'])
+
+    # for now just draw the roi
+    # roi_draw_img = roi_draw(img_in,x,y,w,h)
+ 
+    skinMask = skinDetector.detect(img_in)
+    roi_crop_img = roi_crop(img_in,x,y,w,h)
+    roi_crop_skinMask = roi_crop(skinMask,x,y,w,h)
+    
+    pro_img = cv2.bitwise_and(img_in, img_in, mask = skinMask)
+    roi_draw_img = roi_draw(pro_img,x,y,w,h)
     # colorspacCoversion
-    img_dict = colorSpaceCovert(roi_img)
+    img_dict = colorSpaceCovert(roi_crop_img)
     # finding mean and median
     for item_name in img_dict:
         cur_img = img_dict[item_name]
-        mean, median = cal_mm(cur_img,skinMask)
+        mean, median = cal_mm(cur_img,roi_crop_skinMask)
         img_dict[item_name] = [mean, median]
         
     seq_arr.append(img_dict)
     # put into time series array 
-    return seq_arr  
+    return seq_arr,roi_draw_img  
 
+def kernel_visualize(img_in):
+    # face detection
+    x,y,w,h = faceDetect(img_in)
 
 #trying out kernel
 
@@ -124,7 +143,9 @@ if __name__ == '__main__':
     #testing out our kernel
     seq_arr = []
     #first 10 picture
-    images = img_arr[0][0:10]
+    images = img_arr[0][0:1]
     for img in images:
         cur_img = cv2.imread(img)
+        cv2.imshow("images", cur_img)
+        cv2.waitKey()
         seq_arr = kernel(cur_img,seq_arr)
